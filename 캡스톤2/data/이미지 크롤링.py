8c3648f15a0268ada416c795e61138758bbe58cb
gayeon <yeonkimkim1117@gmail.com>
2023-10-23 21:56:15 +0900

    image

--- 캡스톤2/data/이미지 크롤링.py
+++ 캡스톤2/data/이미지 크롤링.py
@@ -46,3 +46,53 @@ urls.append(browser.find_element_by_css_selector(".sFlh5c.pT0Scc.iPVvYb").get_at
 
 for url in urls :
     print(urls)
+    
+    
+    
+    
+    
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+browser_path = 'C:/Users/김가연/Desktop/23-2학기/캡스톤2/chromedriver.exe'
+
+browser = webdriver.Chrome(browser_path)
+browser.get("https://www.google.co.kr/imghp?hl=ko&authuser=0&ogbl")
+# 저장할 이미지 url
+for selected_place_name in place_name:
+    urls = []
+
+    elem = browser.find_element_by_name("q")
+    elem.clear()
+
+    elem.send_keys(selected_place_name)
+    elem.send_keys(Keys.RETURN)
+    time.sleep(3)
+    
+    try:
+        browser.find_elements_by_css_selector(".rg_i.Q4LuWd")[0].click()
+        time.sleep(3)
+        urls.append(browser.find_element_by_css_selector(".sFlh5c.pT0Scc.iPVvYb").get_attribute("src"))
+    except NoSuchElementException:
+        print("첫 번째 이미지 요소를 찾을 수 없음")
+
+    time.sleep(3)
+    
+    try:
+        browser.find_elements_by_css_selector(".rg_i.Q4LuWd")[1].click()
+        time.sleep(3)
+        urls.append(browser.find_element_by_css_selector(".sFlh5c.pT0Scc.iPVvYb").get_attribute("src"))
+    except NoSuchElementException:
+        print("두 번째 이미지 요소를 찾을 수 없음")
+
+    time.sleep(3)
+    
+    try:
+        browser.find_elements_by_css_selector(".rg_i.Q4LuWd")[2].click()
+        time.sleep(3)
+        urls.append(browser.find_element_by_css_selector(".sFlh5c.pT0Scc.iPVvYb").get_attribute("src"))
+    except NoSuchElementException:
+        print("세 번째 이미지 요소를 찾을 수 없음")
+    
+    url_string = ', '.join(urls)
+    df.loc[df['미래유산명(장소명)'] == selected_place_name, '이미지'] = url_string
